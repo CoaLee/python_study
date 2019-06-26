7d8c89941997d0621ff1c5bc393f3c093a0fb42a
Yongjae <coaleeyong@gmail.com>
2019-06-27 08:21:43 +0900

    day4 working on

---
+++ day4_compound_loop1/README.md
@@ -0,0 +1,10 @@
+# Lecture4. Compound Data Types, Loop Statement
+## 0. 복습
+- 
+- midterm 풀어보기
+
+## 1. 복합 자료형1: 리스트 자료형
+- 
+
+## 2. 반복문1: 
+
---
+++ day4_compound_loop1/list.py
@@ -0,0 +1,6 @@
+# list.py
+
+listA = [1, 2, 3, 4, 5]
+listB = ['hi', 'Bonjour', 'Xiao', 'Hello']
+listC = [1, '23', 2.4, True, 304 + 12 * 3, 32 > 23 or 12 >= 20]
+
